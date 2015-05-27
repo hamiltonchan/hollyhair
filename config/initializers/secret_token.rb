e2d01f36b5416aaaab224279c4ca771f4c985a40
Hamilton Chan <hamiltonchanjr@gmail.com>
2015-05-27 14:24:42 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Holly::Application.config.secret_key_base = 'f68e725cd37d85d7256d3da2287126f0ed1cd3575aa6d1884db17dedfcc852b91d1d5a0784f26376194c351c08df601fadd9e8a5bf192254fbd64d75a98163f3'
